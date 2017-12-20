01d7056af0d973f63f9fbb6f7171b179d4a01e12
Simon Lee <pair+clee13@pivotallabs.com>
2017-12-20 16:28:33 -0500

    Fix packaging for npm

---
+++ dist/index.js
@@ -0,0 +1,13 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _VueSocket = require('./src/VueSocket');
+
+var _VueSocket2 = _interopRequireDefault(_VueSocket);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+exports.default = new _VueSocket2.default();
